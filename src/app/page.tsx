19733e37b3a415521f21d23570345dada7d22435
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-09-03 23:10:49 +0600

    add: info comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import HeroMain from '@/widgets/HeroMain';
 import HeroMainMobile from '@/widgets/HeroMain/ui/HeroMainMobile';
+import InfoComp from '@/widgets/InfoComp';
 
 export default function Home() {
   return (
-    <main className="flex w-full min-w-dvw min-h-dvh flex-col items-center justify-between">
+    <main className="flex w-full min-w-dvw min-h-dvh flex-col items-center ">
       <div className="bg-[url('@/shared/assets/bg.png')] min-[390px]:pt-[21px]  min-[1280px]:pt-[187px] min-[1280px]:px-[70px] min-[1440px]:px-[146px]  min-[1920px]:pt-[332px] min-[1920px]:px-[283px] bg-cover bg-center  max-w-dvw w-full max-h-dvh min-[390px]:h-[736px] min-[1280px]:h-[757px] min-[1920px]:h-[1058px] hidden min-[1280px]:block">
         <HeroMain />
       </div>
       <div className="bg-[url('@/shared/assets/bg.png')] pb-[15px] w-full min-[390px]:pt-[21px] block min-[1280px]:hidden">
         <HeroMainMobile />
       </div>
+      <InfoComp />
     </main>
   );
 }
---
+++ src/widgets/InfoComp/ui/InfoComp.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const InfoComp = () => {
+  return (
+    <ul className="flex max-[1280px]:flex-col w-full  min-[1280px]items-center gap-[15px] mt-[40] max-[1270px]:px-[24px] max-[1281px]:px-[66px] min-[1280px]:mt-[91px] max-w-[1351px] max-[1920px]:h-[93px] min-[1920]:h-[124px] text-[#2E2C30]">
+      <li className="border-l-2 border-[#EF3824] px-[16px] h-full">
+        <h2 className="text-[34px] min-[1920px]:text-[42px] font-bold text-nowrap">
+          {'>'} 600 проектов
+        </h2>
+        <p className="max-[1920px]:text-[16px] text-[18px]">в России и зарубежом</p>
+      </li>
+      <li className="border-l-2 border-[#EF3824] px-[16px] h-full flex flex-col ">
+        <h2 className="text-[34px] min-[1920px]:text-[42px] font-bold">{'>'} 500 видов</h2>
+        <p className="max-[1920px]:text-[16px] text-[18px]">
+          светильников на собственном заводе в Тульской области
+        </p>
+      </li>
+      <li className="border-l-2 border-[#EF3824] px-[16px] h-full flex flex-col ">
+        <h2 className="text-[34px] min-[1920px]:text-[42px] font-bold text-nowrap">17 лет</h2>
+        <p className="text-nowrap max-[1920px]:text-[16px] text-[18px]">успешных проектов</p>
+      </li>
+      <li className="border-l-2 border-[#EF3824] flex flex-col justify-center min-[391px]:justify-end px-[16px] h-full pb-[16px]">
+        <h2 className="text-[34px] min-[1920px]:text-[42px] font-bold">{''}</h2>
+        <p className="max-[1920px]:text-[16px] text-[18px]">Полный цикл ведения проекта</p>
+      </li>
+      <li className="border-l-2 border-[#EF3824] flex flex-col justify-end px-[16px] h-full pb-[16px]">
+        <h2 className="text-[34px] min-[1920px]:text-[42px] font-bold"></h2>
+        <p className="max-[1920px]:text-[16px] text-[18px]">Документы и сертификаты</p>
+      </li>
+    </ul>
+  );
+};
+
+export default InfoComp;
